4453b4a2a8607eac99352433df237852422052a4
Anton Drevin <a.drevin@mail.ru>
2025-08-14 20:31:17 +0400

    fixes

---
+++ src/views.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+import pandas as pd
+
+from src.utils import cards, currencies, day_time, stock_prices, top_transakpion
+
+
+def get_views(df: pd.DataFrame, user_setting: dict, date_time: str) -> dict:
+    """
+    Основная функция: собирает все данные в итоговый JSON для страницы "Главная".
+    """
+    currency = ",".join(user_setting["user_currencies"])
+    stocks = user_setting["user_stocks"]
+    end_date = datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
+    start_date = datetime.strftime(end_date, "%Y-%m-01")
+
+    df["Дата операции"] = pd.to_datetime(df["Дата операции"], dayfirst=True)
+    df = df[(df["Дата операции"] >= start_date) & (df["Дата операции"] <= end_date)]
+
+    return {
+        "greeting": day_time(),
+        "cards": cards(df),
+        "top_transactions": top_transakpion(df),
+        "currency_rates": currencies(currency),
+        "stock_prices": stock_prices(stocks),
+    }
